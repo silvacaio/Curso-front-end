9158a9b23cad243ab283bd016456f72d15b9f605
silvacaio <silva.caiosfc@hotmail.com>
2018-08-03 00:00:11 -0300

    Adicionado o shop

---
+++ Projeto01/view/footer.php
@@ -0,0 +1,117 @@
+<footer>
+		<div class="row-cinza-claro">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2 text-center hidden-xs">
+						<img class="logotipo" src="img/orlando-logo.png" alt="Logotipo">
+					</div>
+					<div class="col-md-10">
+						<div class="row row-cols">
+							<div class="col-md-4 col-popular-post hidden-xs">
+								<h4>POPULAR POST</h4>
+								<ul class="list-unstyled">
+									<li>
+										<h5>Projeto de teste do curso de front-end</h5>
+									</li>
+									<li>
+										<time>Janeiro, 25, 2018</time>
+									</li>
+									<li>
+										<h5>Projeto de teste do curso de front-end</h5>
+									</li>
+									<li>
+										<time>Janeiro, 25, 2018</time>
+									</li>
+								</ul>
+							</div>
+							<div class="col-md-4 col-links hidden-xs">
+								<h4>LINKS</h4>
+								<ul class="list-unstyled">
+									<li>
+										<a href="#">
+											<i class="fa fa-angle-right"></i>Tickets</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fa fa-angle-right"></i>News</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fa fa-angle-right"></i>Schedule</a>
+									</li>
+								</ul>
+							</div>
+							<div class="col-md-4 col-get-in-touch">
+								<h4 class="hidden-xs">GET IN TOUCH</h4>
+								<address class="hidden-xs">
+									<i class="fa fa-map-marker-alt"></i>
+									<span>R. Borges de Medeiros, 1341
+										<br> Flores da Cunha - RS</span>
+								</address>
+								<p class="hidden-xs">
+									<a href="tel:5499999999">
+										<i class="fa fa-phone"></i>5499999999</a>
+								</p>
+								<div class="row-fluid visible-xs action-buttons">
+									<div class="col-xs-6">
+										<a href="#" class="btn btn-footer ">
+											<i class="fa fa-map-marker-alt"></i>Location</i>
+										</a>
+									</div>
+									<div class="col-xs-6">
+										<a href="#" class="btn btn-footer">
+											<i class="fa fa-phone"></i>Call</i>
+										</a>
+									</div>
+								</div>
+								<ul class="list-unstyled list-socials">
+									<li>
+										<a href="#" target="_blank">
+											<i class="fab fa-facebook-f"></i>
+										</a>
+									</li>
+									<li>
+										<a href="#" target="_blank">
+											<i class="fab fa-instagram"></i>
+										</a>
+									</li>
+									<li>
+										<a href="#" target="_blank">
+											<i class="fab fa-twitter"></i>
+										</a>
+									</li>
+									<li>
+										<a href="#" target="_blank">
+											<i class="fab fa-whatsapp"></i>
+										</a>
+									</li>
+									<li class="btn-up">
+										<a href="#" id="page-up" target="_blank">
+											<i class="fa fa-chevron-up"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="row-cinza-escuro">
+			<div class="container copyright-mobile">
+				<p class="pull-left">Copyright Orlando City Soccer 2018. All rights reserved.</p>
+				<p class="pull-right text-roxo"> Created by CAIO</p>
+			</div>
+		</div>
+	</footer>
+    
+	<script src="lib/jquery/jquery.min.js"></script>
+	<script src="lib/OwlCarousel2/dist/owl.carousel.min.js"></script>
+	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+	<script src="js/efeitos.js"></script>
+	<script type="text/javascript"></script>
+    </body>
+
+</html>
